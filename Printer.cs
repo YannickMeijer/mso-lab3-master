d896977cf8246883e838eee57f5f43d7412d9a9d
iamakittan <tanimeatsixx@gmail.com>
2015-10-06 22:10:13 +0200

    hello

--- Printer.cs
+++ Printer.cs
@@ -2,9 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
-class Printer
+namespace Lab3
 {
-}
+    public class Printer
+    {
+        Ticket ticket;
 
+        public Printer(float price, string clas, string way, string discount, string payment, string startingdes, string enddes)
+        {
+            this.price = ticket.price;
+            this.clas = ticket.clas;
+            this.
+        }
+
+        public void PrintReceipt(UIInfo uii)
+        {
+            Message
+        }
+
+        public void PrintTicket()
+        {
+
+        }
+    }
+}
 
---
+++ Receipt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public class Receipt
+    {
+        UIInfo uiinfo = new UIInfo();
+
+        // Price and Payment are all we need on the receipt.
+        public void ReceiptInfo(float amount)
+        {
+            MessageBox.Show("Price: " + amount + " EUR" + "\n" + 
+                            "Payment: " + uiinfo.Payment);
+        }
+    }
+}
