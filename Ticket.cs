244b15ef74dd246684adaca7d515a8be36bb4a7e
YannickMeijer <meyannick@hotmail.com>
2015-10-05 21:19:00 -0700

    Added classes, they stilll need to be filled in though

---
+++ Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+abstract class Ticket //This class is abstract in case future changes require methods to be added. It is a superclass to StandardTicket
+{
+    String date;
+    String startingStation;
+    String destination;
+    String kindOfTicket;
+    String trainClass;
+    String price;
+}
